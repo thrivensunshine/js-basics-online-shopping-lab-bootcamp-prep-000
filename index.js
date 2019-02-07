1aba0fc82135cbd8be518c9d5cacac6290c200c5
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-07 18:25:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ for(let i = 0; i < cart.length; i++){
   if(ccNum === undefined){
     return `Sorry, we don't have a credit card on file for you.`
   }
-  let fin = `Your total cost is ${total(cart)}, which will be charged to the card ${ccNum}.`
+  let fin = `Your total cost is $${total(cart)}, which will be charged to the card ${ccNum}.`
   cart.slice(0,cart.length)
   return fin
 }

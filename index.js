e0cf9818b414b34b1d8ac4654a22be3d15d97822
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-07 14:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,43 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+//--------------FUNCTION 1- ADD TO CART -------------------------
+
+function addToCart(item){
+//let fin  = `${item} has been added to your cart.`
+//console.log(cart)
+let price = Math.floor(Math.random()*100)
+
+let itemObj = {
+  itemName: item,
+  itemPrice: price
+}
+cart.push(itemObj)
+
+//console.log(item + " has been added to your cart. ")
+return `${item} has been added to your cart.`
 }
 
-function viewCart() {
-  // write your code here
+// --------------FUNCTION 2- VIEW CART -------------------------
+function viewCart(){
+  
+  let partSen = [];
+
+  if(cart.length === 0){
+    return "Your shopping cart is empty.";
+  }
+
+  for(let i = 0; i < cart.length; i++){
+    //console.log( Object.values(cart))
+      let vals = Object.values(cart[i]);
+
+    partSen.push(` ${vals[0]} at $${vals[1]}`);
+    partSen.join(',');
+    //console.log(partSen )
+
+  }return `In your cart, you have${partSen}.`;
 }
+
 
 function total() {
   // write your code here
